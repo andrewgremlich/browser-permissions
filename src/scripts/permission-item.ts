ab8359df9c30c03a28892cbe33c49f25ea3918f0
andrewgremlich <andrew.gremlich@gmail.com>
2023-12-29 20:38:25 -0700

    icons added

--- src/scripts/permission-item.ts
+++ src/scripts/permission-item.ts
@@ -1,11 +1,13 @@
+import { getPermissionIcon } from "./icons";
+import { getPermissionQuery } from "./permissions-helpers";
 import { Permissions } from "./types";
 
 const template = (dataName: string) => {
   return `
-  <p>Request ${dataName}?</p>
+  <p>Request ${dataName}? ${getPermissionIcon(dataName as Permissions)}</p>
   <button type="button" data-name"${dataName}" class="permission-grant">Grant</button>
   `;
-}
+};
 
 export class RequestPermission extends HTMLElement {
   // biome-ignore lint/complexity/noUselessConstructor: This IS needed for HTMLElement inheritance
@@ -17,23 +19,26 @@ export class RequestPermission extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
     const reasonForRequest = this.getAttribute("reason-for-request");
 
-    if (!this.getAttribute('data-name')) {
-      throw new Error('data-name attribute is required on permission-item element.');
+    if (!this.getAttribute("data-name")) {
+      throw new Error(
+        "data-name attribute is required on permission-item element.",
+      );
     }
 
-    shadow.innerHTML = template(this.getAttribute('data-name') ?? '');
+    shadow.innerHTML = template(this.getAttribute("data-name") ?? "");
 
     // there may need to be another component or feature to trigger with the user
     // tries to activate something that requires permissions.
-    const button = shadow.querySelector('.permission-grant');
+    const button = shadow.querySelector(".permission-grant");
 
-    button?.addEventListener('click', () => {
-      this.grantPermission(this.getAttribute('data-name') as Permissions);
+    button?.addEventListener("click", () => {
+      this.grantPermission(this.getAttribute("data-name") as Permissions);
     });
   }
 
   grantPermission(string: Permissions) {
     // trigger browser request for permission.
-    console.log('grantPermission', string)
+    console.log("grantPermission", string);
+    getPermissionQuery(string);
   }
 }
