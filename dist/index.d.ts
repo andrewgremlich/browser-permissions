83ca9fdad8d9b2d1cc622d17983c914da197901e
andrewgremlich <andrew.gremlich@gmail.com>
2024-01-14 16:17:50 -0700

    removing dist from tracking

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Makes the browser-permissions and request-permission web components available.
- * @param permissionsParam - { name: string, reason: string }[] Browser Permissions to request.
- */
-export declare function makeBrowserPermissions(): void;
--- dist/permissions-helpers/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { Permissions, PermissionsResponse } from '../types';
-export declare const getPermissionQuery: (permissionName: Permissions) => (() => Promise<PermissionsResponse>);
